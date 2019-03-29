a9056a9ecfb41230f4f311406bc3b1903b21bae9
funkadelic83 <mikedamanskis@gmail.com>
2019-03-28 20:01:56 -0700

    initial commit

---
+++ train-scheduler/assets/javascript/trains.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCaN5XR0fvTivJQhLBXvxERgfFsN9L9iyQ",
+    authDomain: "trains-51574.firebaseapp.com",
+    databaseURL: "https://trains-51574.firebaseio.com",
+    projectId: "trains-51574",
+    storageBucket: "",
+    messagingSenderId: "398506627878"
+  };
+  firebase.initializeApp(config);
